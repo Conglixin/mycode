3c7122b6678dc39d0d9a46cffb9db1b9a9d091f3
Conglixin <820923470@qq.com>
2019-08-05 14:13:14 +0800

    820923470@qq.com

--- vuecli/pro/src/App.vue
+++ vuecli/pro/src/App.vue
@@ -6,7 +6,20 @@
     <router-view/>
   </div>
 </template>
-
+<script>
+import Navgation from"@/components/Navgation.vue"
+export default {
+  components:{
+    Navgation
+  }
+}
+</script>
 <style>
-
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  a{
+    
+  }
 </style>
